790d34c8a39f010fb1353189bc2fdb1466be2ee4
SaulRet <sau1585@hotmail.com>
2024-06-29 16:45:23 -0600

    Add project files.

---
+++ AP.Data/Repository/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using AP.Data.Models;
+
+namespace AP.Data.Repository;
+
+public interface IUserRepository
+{
+    bool AddEntity(User user);
+}
+
+public class UserRepository(ApdatadbContext context) : Repository<User>(context), IUserRepository
+{
+    public bool AddEntity(User user)
+    {
+        return user == null
+            ? throw new ArgumentNullException("User does not exist!") 
+            : Add(user);
+    }
+}
